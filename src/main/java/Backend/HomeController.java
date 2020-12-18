03c6afc5d0c51a4dcbe5896954d3a6c5b28b8f41
JianHui <hui971105@gmail.com>
2020-12-18 18:08:25 +0800

    Update to the backend

--- src/main/java/Backend/HomeController.java
+++ src/main/java/Backend/HomeController.java
@@ -2,14 +2,24 @@ package Backend;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 public class HomeController {
-    @GetMapping("/")
-    public String Home(){
+    private String answer = null;
+    @GetMapping("/api/result")
+    public String Home(String ans) {
+        return answer;
+    }
+
+    @PostMapping("/api/result")
+    public String postBody(@RequestBody ResultModel result){
         Fuzzylogic fl = new Fuzzylogic();
-        return fl.run((float) 1,(float)0.5,(float)0.66);
+        answer = fl.run((float) result.age, (float) result.score1, (float) result.score2);
+        return answer;
     }
 }
---
+++ src/main/java/Backend/ResultModel.java
@@ -0,0 +1,14 @@
+package Backend;
+
+public class ResultModel {
+    public float age;
+    public float score1;
+    public float score2;
+
+    public ResultModel(float age, float score1, float score2){
+        this.age = age;
+        this.score1 = score1;
+        this.score2 = score2;
+    }
+
+}
