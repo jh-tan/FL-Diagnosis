89d279155ba5be9aa8fba3b50aaff47856ec486b
JianHui <hui971105@gmail.com>
2020-12-28 20:58:14 +0800

    edited the backend routing

--- src/main/java/Backend/HomeController.java
+++ src/main/java/Backend/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
@@ -18,9 +19,11 @@ public class HomeController {
         return answer;
     }
 
-    @RequestMapping(value = "/")
-    public String index() {
-        return "index.html";
+    @RequestMapping(value ="/")
+    public ModelAndView getReportsByCategory() throws Exception{
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
     }
 
     @PostMapping("/api/result")
